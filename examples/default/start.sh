2c4987438087f3eaa9d84f051f04a38ad278ecf2
qinxuanwu <qinxuanwu@yungengxin.com>
2024-06-14 11:45:42 +0800

    adjust start.sh

--- examples/default/start.sh
+++ examples/default/start.sh
@@ -2,15 +2,17 @@
 
 docker-compose up -d
 
-gateway=cr.zsm.io
+# gateway=cr.zsm.io
+gateway=crproxy.myzero1.xyz
 
 declare -A mapping=()
+mapping["docker.${gateway}"]="docker.io"
 
 #./setup-gateway.sh "${gateway}" "registry:5000"
 ./setup-gateway.sh "${gateway}" "crproxy:8080"
 ./update-tls.sh "${gateway}"
 
 for key in ${!mapping[*]}; do
-  ./setup-alias.sh "${key}" "${mapping[$key]}" "${gateway}"
-  ./update-tls.sh "${key}"
+    ./setup-alias.sh "${key}" "${mapping[$key]}" "${gateway}"
+    ./update-tls.sh "${key}"
 done
